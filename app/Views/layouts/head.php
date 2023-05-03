303b54bc08ce1a9c453d4e6926d0fec7561da83c
Rara212 <78282415+Rara212@users.noreply.github.com>
2023-05-03 17:37:48 +0700

    mainpage raw templating

---
+++ app/Views/layouts/head.php
@@ -0,0 +1,23 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Get Goers</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/assets/frontend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="/assets/frontend/assets/css/fontawesome.css">
+    <link rel="stylesheet" href="/assets/frontend/assets/css/templatemo-woox-travel.css">
+    <link rel="stylesheet" href="/assets/frontend/assets/css/owl.css">
+    <link rel="stylesheet" href="/assets/frontend/assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+<!--
+
+TemplateMo 580 Woox Travel
+
+https://templatemo.com/tm-580-woox-travel
+
+-->
