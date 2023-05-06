13424fcb1e3d62116fef32f96f789ba2ae9619ea
ASUS <yusufsulaiman324@gmail.com>
2023-05-06 22:34:36 +0700

    menambahkan folder registration & file details.php

---
+++ app/Views/registration/details.php
@@ -0,0 +1,40 @@
+<?= $this->extend('base') ?>
+<?= $this->section('content') ?>
+<div class="row">
+  <div class="col-lg-8">
+    <div class="items">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="item">
+            <div class="row">
+              <div class="col-lg-4 col-sm-5">
+                <div class="image">
+                  <img src="assets/images/country-01.jpg" alt="">
+                </div>
+              </div>
+              <div class="col-lg-8 col-sm-7">
+                <div class="right-content">
+                  <h4>SWITZERLAND</h4>
+                  <span>Europe</span>
+                  <div class="main-button">
+                    <a href="about.html">Explore More</a>
+                  </div>
+                  <p>Woox Travel is a professional Bootstrap 5 theme HTML CSS layout for your website. You can use this layout for your commercial work.</p>
+                  <ul class="info">
+                    <li><i class="fa fa-user"></i> 8.66 Mil People</li>
+                    <li><i class="fa fa-globe"></i> 41.290 km2</li>
+                    <li><i class="fa fa-home"></i> $1.100.200</li>
+                  </ul>
+                  <div class="text-button">
+                    <a href="about.html">Need Directions ? <i class="fa fa-arrow-right"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>                                                               
+      </div>
+    </div>
+  </div>         
+</div>
+<?= $this->endSection() ?>
