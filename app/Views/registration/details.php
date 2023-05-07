5c58c1bc25ad61d11aabef74eb673fab95e10982
Rara212 <78282415+Rara212@users.noreply.github.com>
2023-05-07 08:01:10 +0700

    edited details.php

--- app/Views/registration/details.php
+++ app/Views/registration/details.php
@@ -1,40 +1,51 @@
 <?= $this->extend('base') ?>
 <?= $this->section('content') ?>
-<div class="row mt-5 pt-5">
-  <div class="col-lg-8">
-    <div class="items">
+  <div class="visit-country">
+    <div class="container">
       <div class="row">
-        <div class="col-lg-12">
-          <div class="item">
+        <div class="col-lg-5">
+          <div class="section-heading">
+            <h2>Event Details</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="items">
             <div class="row">
-              <div class="col-lg-4 col-sm-5">
-                <div class="image">
-                  <img src="/assets/frontend/assets/images/country-01.jpg" alt="">
-                </div>
-              </div>
-              <div class="col-lg-8 col-sm-7">
-                <div class="right-content">
-                  <h4>SWITZERLAND</h4>
-                  <span>Europe</span>
-                  <div class="main-button">
-                    <a href="about.html">Explore More</a>
-                  </div>
-                  <p>Woox Travel is a professional Bootstrap 5 theme HTML CSS layout for your website. You can use this layout for your commercial work.</p>
-                  <ul class="info">
-                    <li><i class="fa fa-user"></i> 8.66 Mil People</li>
-                    <li><i class="fa fa-globe"></i> 41.290 km2</li>
-                    <li><i class="fa fa-home"></i> $1.100.200</li>
-                  </ul>
-                  <div class="text-button">
-                    <a href="about.html">Book Now <i class="fa fa-arrow-right"></i></a>
+              <div class="col-lg-12">
+                <div class="item">
+                  <div class="row">
+                    <div class="col-lg-4 col-sm-5">
+                      <div class="image">
+                        <img src="/assets/frontend/assets/images/country-01.jpg" alt="">
+                      </div>
+                    </div>
+                    <div class="col-lg-8 col-sm-7">
+                      <div class="right-content">
+                        <h4>SWITZERLAND</h4>
+                        <span>Europe</span>
+                        <div class="main-button">
+                          <a href="about.html">Book Now</a>
+                        </div>
+                        <p>Woox Travel is a professional Bootstrap 5 theme HTML CSS layout for your website. You can use this layout for your commercial work.</p>
+                        <ul class="info">
+                          <li><i class="fa fa-user"></i> 8.66 Mil People</li>
+                          <li><i class="fa fa-globe"></i> 41.290 km2</li>
+                          <li><i class="fa fa-home"></i> $1.100.200</li>
+                        </ul>
+                        <div class="text-button">
+                          <a href="about.html">Contact info <i class="fa fa-arrow-right"></i></a>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>                                                               
+        </div>
       </div>
     </div>
-  </div>         
-</div>
+  </div>
 <?= $this->endSection() ?>
