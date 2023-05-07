bef13b06956504167cd1809e4616047ed8d473f1
ASUS <aldirzkyrama@gmail.com>
2023-05-07 15:32:14 +0700

    merubah yang ada pada file new.php

--- app/Views/registration/new.php
+++ app/Views/registration/new.php
@@ -17,29 +17,67 @@
                 <div class="item">
                   <div class="row">
                     <!--Ubah nya dari sini -->
-                    <div class="col-lg-4 col-sm-5">
-                      <div class="image">
-                        <img src="/assets/frontend/assets/images/country-01.jpg" alt="">
-                      </div>
-                    </div>
-                    <div class="col-lg-8 col-sm-7">
-                      <div class="right-content">
-                        <h4>SWITZERLAND</h4>
-                        <span>Europe</span>
-                        <div class="main-button">
-                          <a href="about.html">Book Now</a>
+                    <div class="card" style="width: 50rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">Buyer Information</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">Make sure all the information is correct. You cannot change it.</h6>
+                      <form>
+                        <div class="mb-3">
+                          <label for="inputEmail" class="form-label">Email Address</label>
+                          <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+                          <div id="emailHelp" class="form-text">Tickets will be sent to this email</div>
                         </div>
-                        <p>Woox Travel is a professional Bootstrap 5 theme HTML CSS layout for your website. You can use this layout for your commercial work.</p>
-                        <ul class="info">
-                          <li><i class="fa fa-user"></i> 8.66 Mil People</li>
-                          <li><i class="fa fa-globe"></i> 41.290 km2</li>
-                          <li><i class="fa fa-home"></i> $1.100.200</li>
-                        </ul>
-                        <div class="text-button">
-                          <a href="about.html">Contact info <i class="fa fa-arrow-right"></i></a>
+                        <div class="row mb-3">
+                          <div class="col">
+                            <label for="inputFirstName" class="form-label">First Name</label>
+                            <input type="text" class="form-control" id="inputFirstName" placeholder="Enter first name">
+                          </div>
+                          <div class="col">
+                            <label for="inputLastName" class="form-label">Last Name</label>
+                            <input type="text" class="form-control" id="inputLastName" placeholder="Enter last name">
+                          </div>
                         </div>
-                      </div>
+                        <div class="row mb-3">
+                          <div class="col">
+                            <label for="inputCountryCode" class="form-label">Country Code</label>
+                            <select id="inputCountryCode" class="form-select" style="width: 10rem;">
+                              <option selected>+62</option>
+                              <option>+63</option>
+                              <option>+64</option>
+                              <option>+65</option>
+                            </select>
+                          </div>
+                          <div class="col">
+                            <label for="inputPhoneNumber" class="form-label">Phone Number</label>
+                            <input type="text" class="form-control" style="width: 30rem;" id="inputPhoneNumber" placeholder="Enter phone number">
+                          </div>
+                        </div>
+                        <div class="mb-3">
+                          <label for="inputGender" class="form-label">Gender</label>
+                          <div>
+                            <div class="form-check form-check-inline">
+                              <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
+                              <label class="btn btn-primary" style="width: 15rem;" for="option1">Male</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                              <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
+                              <label class="btn btn-primary" style="width: 15rem;" for="option2">Female</label>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="form-check mb-3">
+                          <input class="form-check-input" type="checkbox" value="" id="agreementCheckbox" required>
+                          <label class="form-check-label" for="agreementCheckbox">
+                            I agree with Goers' terms and conditions and privacy policy. Accept agreement and click continue to process your order.
+                          </label>
+                          <div class="invalid-feedback">You must agree before submitting.</div>
+                        </div>
+                        <div class="col-12">
+                          <button type="submit" class="btn btn-primary" style="width: 100%;">Continue</button>
+                        </div>
+                      </form>
                     </div>
+                  </div>
                     <!--Sampai sini aja -->
                   </div>
                 </div>
