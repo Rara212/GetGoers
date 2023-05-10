baeb66036339662e112bd69df82d4012eeed61bd
Rara212 <78282415+Rara212@users.noreply.github.com>
2023-05-10 13:39:04 +0700

    moved new.php to index.php in registration view

--- app/Views/registration/index.php
+++ app/Views/registration/index.php
@@ -5,7 +5,7 @@
       <div class="row">
         <div class="col-lg-5">
           <div class="section-heading">
-            <h2>Event Details</h2>
+            <h2>Booking Page</h2>
           </div>
         </div>
       </div>
@@ -16,29 +16,85 @@
               <div class="col-lg-12">
                 <div class="item">
                   <div class="row">
-                    <div class="col-lg-4 col-sm-5">
-                      <div class="image">
-                        <img src="/assets/frontend/assets/images/country-01.jpg" alt="">
-                      </div>
-                    </div>
-                    <div class="col-lg-8 col-sm-7">
-                      <div class="right-content">
-                        <h4><?= $data['name'] ?></h4>
-                        <span>Europe</span>
-                        <div class="main-button">
-                          <a href="/registration/new">Book Now</a>
+                    <!--Ubah nya dari sini -->
+                    <div class="card" style="width: 50rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">Buyer Information</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">Make sure all the information is correct. You cannot change it.</h6>
+                      <form>
+                        <div class="mb-3">
+                          <label for="inputEmail" class="form-label">Email Address</label>
+                          <input type="email" class="form-control" name="emailUser" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+                          <div id="emailHelp" class="form-text">Tickets will be sent to this email</div>
                         </div>
-                        <p>Woox Travel is a professional Bootstrap 5 theme HTML CSS layout for your website. You can use this layout for your commercial work.</p>
-                        <ul class="info">
-                          <li><i class="fa fa-user"></i> 8.66 Mil People</li>
-                          <li><i class="fa fa-globe"></i> 41.290 km2</li>
-                          <li><i class="fa fa-home"></i> $1.100.200</li>
-                        </ul>
-                        <div class="text-button">
-                          <a href="about.html">Contact info <i class="fa fa-arrow-right"></i></a>
+                        <div class="row mb-3">
+                          <div class="col">
+                            <label for="inputFirstName" class="form-label">First Name</label>
+                            <input type="text" class="form-control" name="firstName" id="inputFirstName" placeholder="Enter first name">
+                          </div>
+                          <div class="col">
+                            <label for="inputLastName" class="form-label">Last Name</label>
+                            <input type="text" class="form-control" name="lastName" id="inputLastName" placeholder="Enter last name">
+                          </div>
                         </div>
-                      </div>
+                        <div class="row mb-3">
+                          <div class="col">
+                            <label for="inputCountryCode" class="form-label">Country Code</label>
+                            <select name="countryCode" id="inputCountryCode" class="form-select" style="width: 10rem;">
+                              <option selected>+62</option>
+                              <option value="+93">Afghanistan (+93)</option>
+                              <option value="+355">Albania (+355)</option>
+                              <option value="+54">Argentina (+54)</option>
+                              <option value="+880">Bangladesh (+880)</option>
+                              <option value="+673">Brunei Darussalam (+673)</option>
+                              <option value="+55">Brazil (+55)</option>
+                              <option value="+855">Cambodia (+855)</option>
+                              <option value="+1">Canada (+1)</option>
+                              <option value="+57">Colombia (+57)</option>
+                              <option value="+45">Denmark (+45)</option>
+                              <option value="+20">Egypt (+20)</option>
+                              <option value="+33">France (+33)</option>
+                              <option value="+299">Greenland (+299)</option>
+                              <option value="+852">Hong Kong (+852)</option>
+                              <option value="+62">Indonesia (+62)</option>
+                            </select>
+                          </div>
+                          <div class="col">
+                            <label for="inputPhoneNumber" class="form-label">Phone Number</label>
+                            <input type="text" class="form-control" style="width: 30rem;" name="telephone" id="inputPhoneNumber" placeholder="Enter phone number">
+                          </div>
+                        </div>
+                        <div class="mb-3">
+                          <label for="inputGender" class="form-label">Gender</label>
+                          <div>
+                            <div class="form-check form-check-inline">
+                              <input type="radio" class="btn-check" name="genderMale" id="option1" autocomplete="off" checked>
+                              <label class="btn btn-primary" style="width: 15rem;" for="option1">Male</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                              <input type="radio" class="btn-check" name="genderFemale" id="option2" autocomplete="off">
+                              <label class="btn btn-primary" style="width: 15rem;" for="option2">Female</label>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="mb-3">
+                          <label for="inputTicket" class="form-label">Book for</label>
+                          <input type="number" class="form-control" name="ticketsIssued" id="inputEmail" aria-describedby="emailHelp" placeholder="1">
+                        </div>
+                        <div class="form-check mb-3">
+                          <input class="form-check-input" type="checkbox" value="" id="agreementCheckbox" required>
+                          <label class="form-check-label" for="agreementCheckbox">
+                            I agree with GetGoers' terms and conditions and privacy policy. Accept agreement and click continue to process your order.
+                          </label>
+                          <div class="invalid-feedback">You must agree before submitting.</div>
+                        </div>
+                        <div class="col-12">
+                          <button type="submit" class="btn btn-primary" style="width: 100%;">Continue</button>
+                        </div>
+                      </form>
                     </div>
+                  </div>
+                    <!--Sampai sini aja -->
                   </div>
                 </div>
               </div>
