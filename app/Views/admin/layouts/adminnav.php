915870723841bc51e0b7789df4e0e2e3325ea601
Rara212 <78282415+Rara212@users.noreply.github.com>
2023-05-13 17:26:51 +0700

    fixed about bugs

--- app/Views/admin/layouts/adminnav.php
+++ app/Views/admin/layouts/adminnav.php
@@ -13,12 +13,12 @@
                     <ul class="nav">
                         <?php if (session('loggedIn', true)): ?>
                         <li><a href="adminpage">Home</a></li>
-                        <li><a href="#section_2">About</a></li>
+                        <li><a href="/#section_2">About</a></li>
                         <li><a href="admin">Deals</a></li>
                         <li><a href="/logout">Logout</a></li>
                         <?php else: ?>
                         <li><a href="adminpage" class="active">Home</a></li>
-                        <li><a href="#section_2">About</a></li>
+                        <li><a href="/#section_2">About</a></li>
                         <li><a href="admin">Deals</a></li>
                         <li><a href="login">Login</a></li>
                         <li><a href="register">Sign Up</a></li>
